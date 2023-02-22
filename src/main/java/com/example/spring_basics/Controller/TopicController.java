0e7d4c9d69b4ca3a8f2efc3d44f7528dab3daf43
Amitosh <amitosh.verma9@gmail.com>
2023-02-23 00:01:47 +0530

    Normal comments were made for better understanding
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/example/spring_basics/Controller/TopicController.java
+++ src/main/java/com/example/spring_basics/Controller/TopicController.java
@@ -14,26 +14,31 @@ public class TopicController {
     @Autowired
     private TopicService topicService;
 
+    /** READ */
     @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
         return topicService.getAllTopic();
     }
+
     @RequestMapping ("/topics/{id}")
     public Topic getTopic(@PathVariable String id) {
         return topicService.getTopic(id);
     }
 
+    /** CREATE */
     @PostMapping("/topics")
     public String addTopics(@RequestBody Topic topic) {
          topicService.addTopic(topic);
          return "topic added";
     }
 
+    /** UPDATE */
     @PutMapping ("/topics/{id}")
     public void updateTopic(@PathVariable String id, @RequestBody Topic topic) {
         topicService.updateTopic(id, topic);
     }
 
+    /** DELETE */
     @DeleteMapping("/topics/{id}")
     public void deleteTopic(@PathVariable String id) {
         topicService.remove(id);
